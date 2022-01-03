b6c546477fd61a429ea2cb322fa7f1e2dd9a44d4
Ragul K V <76117375+ragul666@users.noreply.github.com>
2022-01-03 20:59:07 +0530

    Add files via upload
    Initial Commit

---
+++ src/components/AddContact.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+class AddContact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+  };
+
+  add = (e) => {
+    e.preventDefault();
+    if (this.state.name === "") {
+      window.alert("Enter The Name Field!");
+      return;
+    }
+    if (this.state.email === "") {
+      window.alert("Enter The Email Field!");
+      return;
+    }
+
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", email: "" });
+  };
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form" onSubmit={this.add}>
+          <div className="field">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </div>
+          <button className="ui button blue">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
